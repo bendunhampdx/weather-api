77ad841ed7d92860f19b5330e7c0991ab18a2162
bendunhampdx <bendunhampdx@gmail.com>
2021-06-28 14:59:54 -0700

    units change for degrees

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
     }
   });
 });
---
+++ src/weather-interface.js
@@ -0,0 +1,1 @@
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&unitsimperial&appid=${process.env.API_KEY}`
